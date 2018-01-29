4bf4e7320a3c2724b64d18c6d86d9397d63649e7
Amit Erandole <amit.erandole@gmail.com>
2018-01-29 18:07:37 +0530

    using the spread operator

--- App.js
+++ App.js
@@ -3,18 +3,28 @@ import { StyleSheet, Text, View, Alert } from "react-native"
 import MyButton from "./components/MyButton"
 
 export default class App extends React.Component {
+  state = {
+    hidden: false
+  }
+
   handleClick(msg) {
-    Alert.alert("MyAlert", "This is important")
+    this.setState({ hidden: !this.state.hidden })
   }
 
   render() {
+    let buttonProps = {
+      style: { marginBottom: 10 },
+      color: "mediumseagreen",
+      onClick: () => this.handleClick("some custom message"),
+      title: "Click my Button"
+    }
+
     return (
       <View style={styles.container}>
-        <MyButton
-          color={this.props.color}
-          onClick={() => this.handleClick("some custom message")}
-          title="Click my button"
-        />
+        <MyButton {...buttonProps} />
+        {!this.state.hidden && (
+          <Text>This is some content I would like to hide</Text>
+        )}
       </View>
     )
   }
